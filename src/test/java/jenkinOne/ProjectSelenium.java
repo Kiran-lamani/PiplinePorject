9441c7c956c78a28d061b566c5ce17ef40dc5781
kiran <kiranlamani5373@gmail.com>
2022-04-04 16:39:36 +0530

    Dicscipation
    new upalod

---
+++ src/test/java/jenkinOne/ProjectSelenium.java
@@ -0,0 +1,25 @@
+package jenkinOne;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ProjectSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("(//span[@class='nav-action-inner'])[2]")).click();
+		System.out.println(driver.getTitle());
+	}
+}
+
+
